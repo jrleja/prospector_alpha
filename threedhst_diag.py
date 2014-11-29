c3e8699fbacfe7af6258de9c1b7332964f33b733
Joel Leja <joel.leja@gmail.com>
2014-11-29 12:56:36 -0500

    plotting machinery update
    many changes, much update

--- threedhst_diag.py
+++ threedhst_diag.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import triangle
 import pickle
-from bsfh import read_results
+from bsfh import read_results,model_setup
 
 
 def show_chain(sample_results,outname=None,alpha=0.6):
@@ -50,17 +50,56 @@ def show_chain(sample_results,outname=None,alpha=0.6):
 		os.system("open "+outname)
 		plt.close()
 
-#def return_percentiles(sample_results, start=0, thin=1):
+def comp_samples(thetas, model, sps, inlog=True, photflag=0):
+    specvecs =[]
+    obs, _, marker = read_results.obsdict(model.obs, photflag)
+    wave, ospec, mask = obs['wave_effective'], obs['spectrum'], obs['mask']
+    mwave, mospec = wave[mask], ospec[mask]
+    mounc = obs['maggies_unc'][mask]
 
-	#flatchain = sample_results['chain'][:,start::thin,:]
+    if inlog and (photflag == 0):
+         mospec = np.exp(mospec)
+         mounc *= mospec
 
-	#samples = flatchain[:, :, :].reshape((-1, ndim))
-	#samples[:, 2] = np.exp(samples[:, 2])
-	#m_mcmc, b_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
-    #                         	 zip(*np.percentile(samples, [16, 50, 84],axis=0)))
+    for theta in thetas:
+        mu, cal, delta, mask, wave = read_results.model_comp(theta, model, sps,
+                                           inlog=True, photflag=1)
+
+        if inlog & (photflag == 0):
+            full_cal = np.exp(cal + delta)
+            mod = np.exp(mu + cal + delta)
+            mu = np.exp(mu)
+            cal = np.exp(cal)
+            
+        elif photflag == 0:
+            full_cal = cal + delta/mu
+            mod = (mu*cal + delta)
+
+        else:
+            mod = mu
+
+        specvecs += [ [mu, cal, delta, mod,mospec/mod, (mospec-mod) / mounc] ]
+            
+    return wave, mospec, mounc, specvecs
+
+def return_mwave_custom(filters):
+
+	loc = '/Users/joel/code/python/threedhst_bsfh/filters/'
+	key_str = 'filter_keys_threedhst.txt'
+	lameff_str = 'lameff_threedhst.txt'
+	
+	lameff = np.loadtxt(loc+lameff_str)
+	keys = np.loadtxt(loc+key_str, dtype='S20',usecols=[1])
+	keys = keys.tolist()
+	keys = np.array([keys.lower() for keys in keys], dtype='S20')
+	
+	lameff_return = [[lameff[keys == filters[i]]][0] for i in range(len(filters))]
+	lameff_return = [item for sublist in lameff_return for item in sublist]
+	
+	return lameff_return
 
 def phot_figure(sample_results, alpha=0.3, samples = [-1],
-                start=0, thin=1,
+                start=0, thin=1, maxprob=0,
                 **kwargs):
 	"""
 	Plot the photometry for the model and data (with error bars). Then
@@ -69,6 +108,8 @@ def phot_figure(sample_results, alpha=0.3, samples = [-1],
 	from matplotlib import gridspec
 	import fsps
 	sps = fsps.StellarPopulation(zcontinuous=True)
+	custom_filter_keys = '/Users/joel/code/python/threedhst_bsfh/filters/filter_keys_threedhst.txt'
+	fsps.filters.FILTERS = model_setup.custom_filter_dict(custom_filter_keys)
 
 	fig = plt.figure()
 	gs = gridspec.GridSpec(2,1, height_ratios=[3,1])
@@ -78,24 +119,38 @@ def phot_figure(sample_results, alpha=0.3, samples = [-1],
 	res.set_ylabel( r'$\chi$')
 	phot.set_ylabel('maggies')
     
-	# posterior draws
+	# random posterior draws
+	# chain = chain[nwalkers,nsteps,ndim]
 	flatchain = sample_results['chain'][:,start::thin,:]
-	flatchain = flatchain.reshape(flatchain.shape[0] * flatchain.shape[1],
-                                  flatchain.shape[2])
+	flatchain = flatchain.reshape(flatchain.shape[0] * flatchain.shape[1],flatchain.shape[2])	
 	thetas = [flatchain[s,:] for s in samples]
-	#mwave, mospec, mounc, specvecs = 
-	mu, specvecs, delta, mask, mwave = read_results.model_comp(thetas, sample_results['model'], sps, photflag=1)
+	mwave, mospec, mounc, specvecs = comp_samples(thetas, sample_results['model'], sps, photflag=1)
 	
-	#print(mwave, mospec)
 	for vecs in specvecs:
 		vv = vecs[0], vecs[-1]
-		[ax.plot(mwave, v, color='magenta', alpha=alpha, marker='o', **kwargs)
+
+		[ax.plot(np.log10(mwave), np.log10(v), color='grey', alpha=alpha, marker='o', label='random sample', **kwargs)
 		for ax, v in zip([phot, res], vv) ]
     
-	phot.errorbar(mwave, mospec, yerr=mounc,
+	phot.errorbar(np.log10(mwave), np.log10(mospec), yerr=mounc,
                   color='black')
-	phot.plot(mwave, mospec, label = 'observed',
+	phot.plot(np.log10(mwave), np.log10(mospec), label = 'observed',
               color='black', marker='o', **kwargs)
+	
+	# max probability
+	if maxprob == 1:
+		thetas = sample_results['chain'][sample_results['lnprobability'] == np.max(sample_results['lnprobability']),:]
+		if thetas.ndim == 2:			# if multiple chains found the same peak, choose one arbitrarily
+			thetas=[thetas[0,:]]
+		else:
+			thetas = [thetas]
+		mwave, mospec, mounc, specvecs = comp_samples(thetas, sample_results['model'], sps, photflag=1)
+		
+		for vecs in specvecs:
+			vv = vecs[0], vecs[-1]
+			[ax.plot(np.log10(mwave), np.log10(v), color='red', marker='o', label='max lnprob', **kwargs)
+			for ax, v in zip([phot, res], vv) ]
+	
 	phot.legend(loc=0)
 	res.axhline(0, linestyle=':', color='grey')
     
@@ -107,28 +162,30 @@ def phot_figure(sample_results, alpha=0.3, samples = [-1],
 def main():
 
 	# define filename and load basics
-	file_base = 'threedhst_1416969772'
-	file_base = 'threedhst_1416974087'
+	file_base = 'threedhst_1417150026'
 	filename="/Users/joel/code/python/threedhst_bsfh/results/"+file_base+"_mcmc"
 	model_filename="/Users/joel/code/python/threedhst_bsfh/results/"+file_base+"_model"
 	sample_results, powell_results, model = read_results.read_pickles(filename, model_file=model_filename,inmod=None)
-    
+
     # chain plot
-	show_chain(sample_results, 
-			   outname='/Users/joel/code/python/threedhst_bsfh/results/'+file_base+"_chain.png")
+	#show_chain(sample_results, 
+	#		   outname='/Users/joel/code/python/threedhst_bsfh/results/'+file_base+"_chain.png",
+	#		   alpha=0.3)
 
 	# triangle plot
-	read_results.subtriangle(sample_results,
-							 outname='/Users/joel/code/python/threedhst_bsfh/results/'+file_base,
-							 showpars=None,start=0, thin=1, truths=sample_results['initial_theta'], show_titles=True)
-	
+	#read_results.subtriangle(sample_results,
+	#						 outname='/Users/joel/code/python/threedhst_bsfh/results/'+file_base,
+	#						 showpars=None,start=0, thin=1, truths=sample_results['initial_theta'], show_titles=True)
+
 	# best-fit model plot
 	# sample
 	nsample = 5
 	ns = sample_results['chain'].shape[0] * sample_results['chain'].shape[1]
 	samples = np.random.uniform(0, 1, size=nsample)
 	sample = [int(s * ns) for s in samples]
- 	
+
  	# plot
- 	pfig = phot_figure(sample_results, samples=sample)
+ 	pfig = phot_figure(sample_results, samples=sample, maxprob=1)
+ 	pfig.savefig('results/sed.png')
+
 	
