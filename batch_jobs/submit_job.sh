e8520561ff1fbdeb8deb9f7370f08af51c660fd0
Joel Leja <joel.leja@gmail.com>
2014-12-09 16:12:35 -0500

    updates batch

--- batch_jobs/submit_job.sh
+++ batch_jobs/submit_job.sh
@@ -9,9 +9,9 @@
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=2000
 ### Job name
-#SBATCH -J '3D_run$SLURM_ARRAY_TASK_ID'
+#SBATCH -J '3D_run1'
 ### output and error logs
-#SBATCH -o mpitest_$SLURM_ARRAY_TASK_ID.out
-#SBATCH -e mpitest_$SLURM_ARRAY_TASK_ID.err
+#SBATCH -o mpitest_%j.out
+#SBATCH -e mpitest_%j.err
 ### source activate pympi
-mpirun -n 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/threedhst_params_$SLURM_ARRAY_TASK_ID.py --sps=fsps --custom_filter_keys=$APPS/threedhst_bsfh/filters/filter_keys_threedhst.txt 
+mpirun -n 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/threedhst_params.py --sps=fsps --custom_filter_keys=$APPS/threedhst_bsfh/filters/filter_keys_threedhst.txt 
