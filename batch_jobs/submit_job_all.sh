cb0c7c422118cf1c836a79316927f98c71298358
Joel Leja <joel.leja@gmail.com>
2014-12-11 16:02:35 -0500

    param file change

--- batch_jobs/submit_job_all.sh
+++ batch_jobs/submit_job_all.sh
@@ -5,7 +5,7 @@
 ### Requested number of nodes
 #SBATCH -N 1
 ### Requested computing time in minutes
-#SBATCH -t 60
+#SBATCH -t 120
 ### Partition or queue name
 #SBATCH -p conroy
 ### memory per cpu, in MB
