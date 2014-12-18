507e0a3ff7eeb6f4588847a6278959e0018056fb
Joel Leja <joel.leja@gmail.com>
2014-12-18 10:36:17 -0500

    further updates

---
+++ batch_jobs/stagger_submission.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script to submit multiple jobs
+# over a specified amount of time
+
+NJOBS=108
+JOBS_PER_BATCH=18
+NBATCH=$(($NJOBS/$JOBS_PER_BATCH))
+DELTAT='150m'
+
+echo 'Submitting '$NJOBS' jobs, with '$JOBS_PER_BATCH' jobs per batch in '$NBATCH' batches.'
+echo 'Waiting '$DELTAT' between submissions'
+
+for ((  i = 1 ;  i <= $NBATCH;  i++  ))
+do
+	
+	JOBSTART=$((($i-1)*$JOBS_PER_BATCH+1))
+	JOBEND=$(($i*$JOBS_PER_BATCH))
+	
+	sbatch --array=$JOBSTART-$JOBEND submit_job_all.sh
+
+	echo 'submitting batch set '$i', jobs '$JOBSTART'-'$JOBEND
+	
+	sleep $DELTAT
+	
+done
--- batch_jobs/submit_job_all.sh
+++ batch_jobs/submit_job_all.sh
@@ -5,7 +5,7 @@
 ### Requested number of nodes
 #SBATCH -N 1
 ### Requested computing time in minutes
-#SBATCH -t 120
+#SBATCH -t 360
 ### Partition or queue name
 #SBATCH -p conroy,general
 ### memory per cpu, in MB
@@ -19,4 +19,4 @@
 #SBATCH --mail-type=ALL 
 #SBATCH --mail-user=joel.leja@gmail.com
 ### source activate pympi
-mpirun -n 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/threedhst_params_$SLURM_ARRAY_TASK_ID.py --sps=fsps --custom_filter_keys=$APPS/threedhst_bsfh/filters/filter_keys_threedhst.txt 
+mpirun -n 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/halpha_selected_params_$SLURM_ARRAY_TASK_ID.py --sps=fsps --custom_filter_keys=$APPS/threedhst_bsfh/filters/filter_keys_threedhst.txt 
