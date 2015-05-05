7694ac61c58b65f6dd69b00362524dad6fbf555f
Joel Leja <joel.leja@gmail.com>
2015-05-05 13:36:54 -0400

    new batch job scripts

---
+++ batch_jobs/testsed_tlink_secondary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 120
+### Partition or queue name
+#SBATCH -p conroy,general
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=3000
+### Job name
+#SBATCH -J 'dtausec'
+### output and error logs
+#SBATCH -o testsed_tlink_sec_%a.out
+#SBATCH -e testsed_tlink_sec_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+### source activate pympi
+python $APPS/threedhst_bsfh/code/extra_output.py $APPS/threedhst_bsfh/parameter_files/testsed_tlink/testsed_tlink_params_$SLURM_ARRAY_TASK_ID.py 
