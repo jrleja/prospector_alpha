49babf17756ea1121b46fd14f1d820c4123494d2
Joel Leja <joel.leja@gmail.com>
2015-05-18 00:07:30 -0400

    new stampede submission scripts, since job arrays are not supported

---
+++ batch_jobs/stampede/onek_run/onek_run_suball_525.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+### Name of the job 
+### Requested number of nodes
+#SBATCH -n 32
+### Requested computing time in minutes
+#SBATCH -t 2:00:00
+###partition
+#SBATCH -p normal
+### Job name
+#SBATCH -J '3Drun'
+### output and error logs
+#SBATCH -o onek_run_525.out
+#SBATCH -e onek_run_525.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+ibrun -np 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/onek_run/onek_run_params_525.py  --sps=fsps --custom_filter_ke
+ys=$APPS/threedhst_bsfh/filters/filter_keys_threedhst.txt 
