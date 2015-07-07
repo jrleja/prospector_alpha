49aa800cf0c22c3f968c44756c2646a730854282
Joel Leja <joel.leja@gmail.com>
2015-07-07 11:32:06 -0400

    set priors such that sf_trunc < tage at all times

--- parameter_files/testsed_simha_newmin/testsed_simha_newmin_params_51.py
+++ parameter_files/testsed_simha_newmin/testsed_simha_newmin_params_51.py
@@ -388,9 +388,11 @@ model_params.append({'name': 'phot_jitter', 'N': 1,
                         'prior_function':tophat,
                         'prior_args': {'mini':5.0, 'maxi':5.0}})
 
-# reset sf_trunc_init to be equal to tage
+# restrict sf_trunc to be less than tage,
+# and set initial to just less than that value
 parmlist = [p['name'] for p in model_params]
-model_params[parmlist.index('sf_trunc')]['init'] = model_params[parmlist.index('tage')]['init']
+model_params[parmlist.index('sf_trunc')]['prior_args']['maxi'] = model_params[parmlist.index('tage')]['init']-0.01
+model_params[parmlist.index('sf_trunc')]['init'] = model_params[parmlist.index('tage')]['init']-0.02
 
 # name outfile
 run_params['outfile'] = run_params['outfile']+'_'+run_params['objname']
