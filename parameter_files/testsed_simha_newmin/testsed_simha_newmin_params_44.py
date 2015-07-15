892a7d59ac26781c1b89af5ce877933c83e609d8
Joel Leja <joel.leja@gmail.com>
2015-07-15 19:45:43 -0400

    updated mocks for new FSPS version, and added buffer of 1.0 Gyr to edge cases

--- parameter_files/testsed_simha_newmin/testsed_simha_newmin_params_44.py
+++ parameter_files/testsed_simha_newmin/testsed_simha_newmin_params_44.py
@@ -120,7 +120,7 @@ class BurstyModel(sedmodel.CSPModel):
             start,end = self.theta_index['sf_start']
             sf_start = theta[start:end]
             if (sf_trunc > self.params['tage']) or \
-               (sf_trunc < sf_start+0.5):
+               (sf_trunc < sf_start+1.0):
                 return -np.inf
 
 
