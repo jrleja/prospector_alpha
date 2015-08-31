94999ce9a5bb5e54eb2a63e5c054e1423088d53b
Joel Leja <joel.leja@gmail.com>
2015-08-31 10:40:11 -0400

    actual final update for logzsol run

---
+++ batch_jobs/brownseds_logzsol_suball.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 32
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 600
+### Partition or queue name
+#SBATCH -p conroy,general
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=2500
+### Job name
+#SBATCH -J '3Drun'
+### output and error logs
+#SBATCH -o brownseds_logzsol_%a.out
+#SBATCH -e brownseds_logzsol_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+### source activate pympi
+mpirun -n 32 python $APPS/bsfh/demo/prospectr.py --param_file=$APPS/threedhst_bsfh/parameter_files/brownseds_logzsol/brownseds_logzsol_params_$SLURM_ARRAY_TASK_ID.py  --sps=fsps 
---
+++ batch_jobs/old_sub/photerr_secondary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 120
+### Partition or queue name
+#SBATCH -p conroy,general
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=2000
+### Job name
+#SBATCH -J 'photerr_sec_%a'
+### output and error logs
+#SBATCH -o photerr_sec_%a.out
+#SBATCH -e photerr_sec_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+### source activate pympi
+python $APPS/threedhst_bsfh/code/extra_output.py $APPS/threedhst_bsfh/parameter_files/photerr/photerr_params_$SLURM_ARRAY_TASK_ID.py 
