fix normalization in halpha_mocks

code/halpha_mocks.py
@@ -77,8 +77,7 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 	'''
 
 	#### output names ####
-	if outname == None:
-		outname = '/Users/joel/code/python/threedhst_bsfh/data/'+basename
+	outname = '/Users/joel/code/python/threedhst_bsfh/data/'+basename
 	parmfile='/Users/joel/code/python/threedhst_bsfh/parameter_files/'+basename+'/'+basename+'_params.py'
 
 	#### load test model, build sps  ####
@@ -87,32 +86,24 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 	sps = threed_dutils.setup_sps(custom_filter_key=None)
 
 	#### basic parameters ####
-	ngals_per_model     = 100
-	noise               = 0.01            # perturb fluxes
-	reported_noise      = 0.01            # reported noise
+	# hack to make it run 100x times in for-loop
+	# so i can calculate sfr each time
+	ngals_per_model     = 1
+	noise               = 0.02            # perturb fluxes
+	reported_noise      = 0.02            # reported noise
 	test_sfhs           = [1,2,3,4,5]     # which test sfhs to use?
-	test_sfhs           = [0]
+	test_sfhs           = np.zeros(100)
 	ntest               = len(test_sfhs)
 	ngals               = ntest*ngals_per_model
 	time_of_trunc       = 0.08 # in Gyr. this is 80 Myr currently
-	
-	#### band-specific noise ####
-	if 'gp_filter_amps' in model.free_params:
-		band_specific_noise = [0.0,0.15,0.25] # add band-specific noise?
-
-	#### outlier noise ####
-	if 'gp_outlier_locs' in model.free_params:
-		outliers_noise      = 0.5             # add outlier noise
-		outliers_bands      = [5,22,29]
-	else:
-		outliers_bands=[]
 
 	#### generate random model parameters ####
 	nparams = len(model.initial_theta)
 	testparms = np.zeros(shape=(ngals,nparams))
 	parnames = np.array(model.theta_labels())
 
-	for jj in xrange(ntest):
+	jj = 0
+	while jj < ntest:
 		for ii in xrange(nparams):
 			
 			#### random in logspace for mass
@@ -131,8 +122,9 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 
 			#### set delt_trunc so that SFH truncates a specific amount of time before observation
 			elif parnames[ii] == 'delt_trunc':
-				tage = np.squeeze(testparms[:,parnames == 'tage'])
-				testparms[jj*ngals_per_model:(jj+1)*ngals_per_model,ii] = (tage-time_of_trunc)/tage
+				for kk in xrange(jj*ngals_per_model,(jj+1)*ngals_per_model): 
+					tage = np.squeeze(testparms[kk,parnames == 'tage'])
+					testparms[kk,ii] = (tage-time_of_trunc)/tage
 
 			#### choose reasonable amounts of dust ####
 			elif parnames[ii] == 'dust2':
@@ -157,8 +149,16 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 				min = model.theta_bounds()[ii][0]
 				max = model.theta_bounds()[ii][1]
 				for kk in xrange(jj*ngals_per_model,(jj+1)*ngals_per_model): testparms[kk,ii] = random.random()*(max-min)+min
-			print parnames[ii]
-			print min,max
+			#print parnames[ii]
+			#print min,max
+
+		sfh_params = threed_dutils.find_sfh_params(model,testparms[jj,:],obs,sps)
+		ssfr_10 = threed_dutils.calculate_sfr(sfh_params, 0.01, minsfr=-np.inf, maxsfr=np.inf)/sfh_params['mformed']
+		if ssfr_10 > 1e-12:
+			print ssfr_10
+			jj += 1
+		else:
+			print 'fail, retry'
 
 	#### make sure priors are satisfied
 	for ii in xrange(ngals):
@@ -187,7 +187,7 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 	#### generate photometry, add noise ####
 	for ii in xrange(ngals):
 		model.initial_theta = testparms[ii,:]
-		_,maggiestemp,_ = model.mean_model(model.initial_theta, obs, sps=sps,norm_spec=False)
+		_,maggiestemp,_ = model.mean_model(model.initial_theta, obs, sps=sps)
 		maggies[ii,:] = maggiestemp
 
 		#### record noise ####
@@ -197,19 +197,8 @@ def ha_mocks(basename,outname=None,add_zp_err=False):
 		for kk in xrange(nfilters): 
 			
 			###### general noise
-			tnoise = noise
-			
-			##### linked filter noise
-			filtlist = model.params.get('gp_filter_locs',[])			
-			for mm in xrange(len(filtlist)):
-				if obs['filters'][kk].lower() in filtlist[mm]:
-					tnoise = (tnoise**2+band_specific_noise[mm]**2)**0.5
-
-			##### outlier noise
-			if kk in outliers_bands:
-				tnoise = (tnoise**2+outliers_noise**2)**0.5
-			add_noise = random.gauss(0, tnoise)
-			print obs['filters'][kk].lower()+': ' + "{:.2f}".format(add_noise)
+			add_noise = random.gauss(0, noise)
+			print obs['filters'][kk].lower()+': ' + "{:.3f}".format(add_noise)
 			maggies[ii,kk] += add_noise*maggies[ii,kk]
 
 	#### output ####

code/threedhst_diag.py
@@ -637,7 +637,7 @@ def sed_figure(sample_results, sps, model,
 	res.plot(wave_eff, chi, 
 		     color=most_likely_color, marker='o', linestyle=' ', label='most likely', 
 		     ms=ms,alpha=alpha,markeredgewidth=0.7,**kwargs)
-	
+
 	nz = modspec > 0
 	phot.plot(modlam[nz], modspec[nz], linestyle='-',
               color=most_likely_color, alpha=0.9,**kwargs)
@@ -752,8 +752,8 @@ def sed_figure(sample_results, sps, model,
 	chisq=np.sum(chi**2)
 	chisq_median=np.sum(median_chi**2)
 	ndof = np.sum(sample_results['obs']['phot_mask']) - len(sample_results['model'].free_params)-1
-	reduced_chisq = chisq/(ndof-1)
-	reduced_chisq_median = chisq_median/(ndof-1)
+	reduced_chisq = chisq/(ndof)
+	reduced_chisq_median = chisq_median/(ndof)
 
 	# also calculate for truths if truths exist
 	if truths is not None:

parameter_files/ha_80myr/ha_80myr_params_12.py
@@ -29,84 +29,73 @@ run_params = {'verbose':True,
 # OBS
 #############
 
-def translate_filters(bfilters, full_list = False):
-    '''
-    translate filter names to FSPS standard
-    this is ALREADY a mess, clean up soon!
-    suspect there are smarter routines to do this in python-fsps
-    '''
+def ret_joel_filts():
 
     # this is necessary for my code
     # to calculate effective wavelength
     # in threed_dutils
-    translate = {
-    'FUV': 'GALEX FUV',
-    'UVW2': 'UVOT w2',
-    'UVM2': 'UVOT m2',
-    'NUV': 'GALEX NUV',
-    'UVW1': 'UVOT w1',
-    'umag': 'SDSS Camera u Response Function, airmass = 1.3 (June 2001)',
-    'gmag': 'SDSS Camera g Response Function, airmass = 1.3 (June 2001)',
-    'rmag': 'SDSS Camera r Response Function, airmass = 1.3 (June 2001)',
-    'imag': 'SDSS Camera i Response Function, airmass = 1.3 (June 2001)',
-    'zmag': 'SDSS Camera z Response Function, airmass = 1.3 (June 2001)',
-    'Jmag': '2MASS J filter (total response w/atm)',
-    'Hmag': '2MASS H filter (total response w/atm)',
-    'Ksmag': '2MASS Ks filter (total response w/atm)',
-    'W1mag': 'WISE W1',
-    '[3.6]': 'IRAC Channel 1',
-    '[4.5]': 'IRAC Channel 2',
-    'W2mag': 'WISE W2',
-    '[5.8]': 'IRAC Channel 3',
-    '[8.0]': 'IRAC CH4',
-    'W3mag': 'WISE W3',
-    "W4'mag": 'WISE W4',
-    '[24]': 'MIPS 24um',
-    'pacs70': 'Herschel PACS 70um',
-    'pacs100': 'Herschel PACS 100um',
-    'pacs160': 'Herschel PACS 160um',
-    'spire250': 'Herschel SPIRE 250um',
-    'spire350': 'Herschel SPIRE 350um',
-    'spire500': 'Herschel SPIRE 500um'
-    }
+    JOEL_FILTNAMES = [
+    'GALEX FUV', 
+    'GALEX NUV', 
+    'SDSS Camera u Response Function, airmass = 1.3 (June 2001)',
+    'SDSS Camera g Response Function, airmass = 1.3 (June 2001)',
+    'SDSS Camera r Response Function, airmass = 1.3 (June 2001)',
+    'SDSS Camera i Response Function, airmass = 1.3 (June 2001)',
+    'SDSS Camera z Response Function, airmass = 1.3 (June 2001)',
+    '2MASS J filter (total response w/atm)',
+    '2MASS H filter (total response w/atm)',
+    '2MASS Ks filter (total response w/atm)',
+    'WISE W1',
+    'IRAC Channel 1',
+    'IRAC Channel 2',
+    'WISE W2',
+    'IRAC Channel 3',
+    'IRAC CH4',
+    'WISE W3',
+    'WISE W4',
+    'MIPS 24um',
+    'Herschel PACS 70um',
+    'Herschel PACS 100um',
+    'Herschel PACS 160um',
+    'Herschel SPIRE 250um',
+    'Herschel SPIRE 350um',
+    'Herschel SPIRE 500um'
+    ]
 
+    return JOEL_FILTNAMES
+
+def ret_fsps_filts():
     # this translates filter names
     # to names that FSPS recognizes
-    translate_pfsps = {
-    'FUV': 'GALEX_FUV',
-    'UVW2': 'UVOT_W2',
-    'UVM2': 'UVOT_M2',
-    'NUV': 'GALEX_NUV',
-    'UVW1': 'UVOT_W1',
-    'umag': 'SDSS_u',
-    'gmag': 'SDSS_g',
-    'rmag': 'SDSS_r',
-    'imag': 'SDSS_i',
-    'zmag': 'SDSS_z',
-    'Jmag': '2MASS_J',
-    'Hmag': '2MASS_H',
-    'Ksmag': '2MASS_Ks',
-    'W1mag': 'WISE_W1',
-    '[3.6]': 'IRAC_1',
-    '[4.5]': 'IRAC_2',
-    'W2mag': 'WISE_W2',
-    '[5.8]': 'IRAC_3',
-    '[8.0]': 'IRAC_4',
-    'W3mag': 'WISE_W3',
-    "W4'mag": 'WISE_W4',
-    '[24]': 'MIPS_24',
-    'pacs70': 'PACS_70',
-    'pacs100': 'PACS_100',
-    'pacs160': 'PACS_160',
-    'spire250': 'SPIRE_250',
-    'spire350': 'SPIRE_350',
-    'spire500': 'SPIRE_500'
-    }
+    FSPS_FILTNAMES = [
+    'GALEX_FUV',
+    'GALEX_NUV',
+    'SDSS_u',
+    'SDSS_g',
+    'SDSS_r',
+    'SDSS_i',
+    'SDSS_z',
+    '2MASS_J',
+    '2MASS_H',
+    '2MASS_Ks',
+    'WISE_W1',
+    'IRAC_1',
+    'IRAC_2',
+    'WISE_W2',
+    'IRAC_3',
+    'IRAC_4',
+    'WISE_W3',
+    'WISE_W4',
+    'MIPS_24',
+    'PACS_70',
+    'PACS_100',
+    'PACS_160',
+    'SPIRE_250',
+    'SPIRE_350',
+    'SPIRE_500'
+    ]
 
-    if full_list:
-        return translate.values()
-    else:
-        return np.array([translate[f] for f in bfilters]), np.array([translate_pfsps[f] for f in bfilters])
+    return FSPS_FILTNAMES
 
 
 def load_obs_mock(filename, objnum):
@@ -115,25 +104,31 @@ def load_obs_mock(filename, objnum):
     """
     obs ={}
 
-    with open(filename, 'r') as f:
-        hdr = f.readline().split()
-    dat = np.loadtxt(filename, comments = '#',
-                     dtype = np.dtype([(n, np.float) for n in hdr[1:]]))
-    obj_ind = np.where(dat['id'] == int(objnum))[0][0]
-    
-    # extract fluxes+uncertainties for all objects and all filters
-    flux_fields = [f for f in dat.dtype.names if f[0:2] == 'f_']
-    unc_fields = [f for f in dat.dtype.names if f[0:2] == 'e_']
-    filters = [f[2:] for f in flux_fields]
+    # if the photometric files exist
+    try:
+        with open(filename, 'r') as f:
+            hdr = f.readline().split()
+        dat = np.loadtxt(filename, comments = '#',
+                         dtype = np.dtype([(n, np.float) for n in hdr[1:]]))
+        obj_ind = np.where(dat['id'] == int(objnum))[0][0]
+        
+        # extract fluxes+uncertainties for all objects and all filters
+        flux_fields = [f for f in dat.dtype.names if f[0:2] == 'f_']
+        unc_fields = [f for f in dat.dtype.names if f[0:2] == 'e_']
+        filters = [f[2:] for f in flux_fields]
 
-    # extract fluxes for particular object, converting from record array to numpy array
-    flux = dat[flux_fields].view(float).reshape(len(dat),-1)[obj_ind]
-    unc  = dat[unc_fields].view(float).reshape(len(dat),-1)[obj_ind]
+        # extract fluxes for particular object, converting from record array to numpy array
+        flux = dat[flux_fields].view(float).reshape(len(dat),-1)[obj_ind]
+        unc  = dat[unc_fields].view(float).reshape(len(dat),-1)[obj_ind]
+    except:
+        filters = ret_fsps_filts()
+        flux = np.zeros(len(filters))
+        unc = np.zeros(len(filters))
 
     # define all outputs
     from translate_filter import calc_lameff_for_fsps
-    wave_effective = calc_lameff_for_fsps(translate_filters(filters,full_list=True))
-    phot_mask = np.logical_or(np.logical_or((flux != unc),(flux > 0)),flux != -99.0)
+    wave_effective = calc_lameff_for_fsps(ret_joel_filts())
+    phot_mask = np.ones_like(wave_effective,dtype=bool)
 
     # build output dictionary
     obs['wave_effective'] = wave_effective