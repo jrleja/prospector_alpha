478cd866c8fda60196cdfc2f73e10aa3ac4f1378
Joel Leja <joel.leja@gmail.com>
2016-03-15 14:32:11 -0400

    new ha_10myr run

---
+++ batch_jobs/ha_10myr_secondary.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 480
+### Partition or queue name
+#SBATCH -p conroy,serial_requeue
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=6000
+### Job name
+#SBATCH -J 'ha_10myrsec'
+### output and error logs
+#SBATCH -o ha_10myr_sec_%a.out
+#SBATCH -e ha_10myr_sec_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+### source activate pympi
+python $APPS/threedhst_bsfh/code/extra_output.py $APPS/threedhst_bsfh/parameter_files/ha_10myr/ha_10myr_params_$SLURM_ARRAY_TASK_ID.py 
