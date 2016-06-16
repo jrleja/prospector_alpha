26239197f2dcf627c6b3e3e40631516aeb9871fe
Joel Leja <joel.leja@gmail.com>
2016-06-15 23:13:20 -0400

    update to submit_all supermic script

--- batch_jobs/supermic/nonparametric_mocks/submit_all.sh
+++ batch_jobs/supermic/nonparametric_mocks/submit_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# this is however many you want to submit
-NARRAY=$1
+# this is however many you want to submit, e.g. jobs 3-8
+NSTART=$1
+NARRAY=$2
 
-for ((  i = 1 ;  i <= $NARRAY;  i++  ))
+for ((  i = $NSTART ;  i <= $NEND;  i++  ))
 do
   cmd='qsub suball_'$i'.sh'
   $cmd
