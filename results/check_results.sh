5dada450a715cb9ed72b48cc1a9322f9782204b0
Joel Leja <joel.leja@gmail.com>
2016-08-25 09:33:29 -0400

    new script to check outputs

---
+++ results/check_results.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# script to replicate parameter file
+# for multiple galaxy IDs
+# set location of ID file, base parameter file
+runname=$1
+IDFILE=$APPS"/threedhst_bsfh/data/brownseds_data/photometry/namelist.txt"
+
+# get number of IDs
+NIDS=$(wc -l < "$IDFILE")
+
+# loop
+for ((  i = 1 ;  i <= $NIDS;  i++  ))
+do
+  # read id
+  LINE=$(sed -n "${i}p" "$IDFILE")
+  mod=${LINE//[ ]/*}
+  if ls $runname/*$mod* 1> /dev/null 2>&1; then
+    echo $i
+  fi
+
+done
