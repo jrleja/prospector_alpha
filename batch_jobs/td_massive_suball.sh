affa47f233df96eed042587aca402086fa2aae20
Joel Leja <joel.leja@gmail.com>
2017-03-09 10:44:58 -0500

    td massive parameter files and runscripts

---
+++ batch_jobs/td_massive_suball.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 32
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 1440
+### Partition or queue name
+#SBATCH -p conroy,general,conroy-intel
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=3000
+### Job name
+#SBATCH -J 'bseds_np'
+### output and error logs
+#SBATCH -o td_massive_%a.out
+#SBATCH -e td_massive_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+srun -n $SLURM_NTASKS --mpi=pmi2 python $APPS/bsfh/scripts/prospector.py --param_file=$APPS/threedhst_bsfh/parameter_files/td_massive/td_massive_params_$SLURM_ARRAY_TASK_ID.py 
