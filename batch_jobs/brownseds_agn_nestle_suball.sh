2c13a89c2e93cc2bd871aec38220d9956f49ee96
Joel Leja <joel.leja@gmail.com>
2017-04-04 16:22:31 -0400

    nestle test

---
+++ batch_jobs/brownseds_agn_nestle_suball.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 32
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 1440
+### Partition or queue name
+#SBATCH -p conroy,general,conroy-intel
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=3000
+### Job name
+#SBATCH -J 'bseds_nest'
+### output and error logs
+#SBATCH -o brownseds_agn_nestle_%a.out
+#SBATCH -e brownseds_agn_nestle_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+srun -n $SLURM_NTASKS --mpi=pmi2 python $APPS/bsfh/scripts/prospector_nest.py --param_file=$APPS/threedhst_bsfh/parameter_files/brownseds_agn_nestle/brownseds_agn_nestle_params_$SLURM_ARRAY_TASK_ID.py 
