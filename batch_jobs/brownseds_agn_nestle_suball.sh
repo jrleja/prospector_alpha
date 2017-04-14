5c34772c97e57ca193d65eeafb4e5a99990d83ad
Joel Leja <joel.leja@gmail.com>
2017-04-14 12:10:13 -0400

    more script updates

--- batch_jobs/brownseds_agn_nestle_suball.sh
+++ batch_jobs/brownseds_agn_nestle_suball.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ### Name of the job
 ### Requested number of cores
-#SBATCH -n 32
+#SBATCH -n 1
 ### Requested number of nodes
 #SBATCH -N 1
 ### Requested computing time in minutes
 #SBATCH -t 1440
 ### Partition or queue name
-#SBATCH -p conroy,general,conroy-intel
+#SBATCH -p conroy-intel,conroy,general,serial_requeue
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=3000
 ### Job name
-#SBATCH -J 'bseds_nest'
+#SBATCH -J 'brownnest'
 ### output and error logs
 #SBATCH -o brownseds_agn_nestle_%a.out
 #SBATCH -e brownseds_agn_nestle_%a.err
@@ -20,7 +20,6 @@
 #SBATCH --mail-user=joel.leja@gmail.com
 IDFILE=$APPS"/threedhst_bsfh/data/brownseds_agn.ids"
 OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
-srun -n $SLURM_NTASKS --mpi=pmi2 \
 python $APPS/bsfh/scripts/prospector_nest.py \ 
 --param_file=$APPS/threedhst_bsfh/parameter_files/brownseds_agn_nestle/brownseds_agn_nestle_params.py \
 --objname=$OBJID \
