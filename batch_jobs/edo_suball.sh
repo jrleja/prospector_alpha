9ee0ea54a2f0745cba05095e059a645a293a5021
Joel Leja <joel.leja@gmail.com>
2017-06-20 19:33:54 -0400

    add parameter file for edo's galaxy

---
+++ batch_jobs/edo_suball.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 32
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 1440
+### Partition or queue name
+#SBATCH -p conroy,general,conroy-intel
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=3000
+### Job name
+#SBATCH -J 'edo'
+### output and error logs
+#SBATCH -o edo_%a.out
+#SBATCH -e edo_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+srun -n $SLURM_NTASKS --mpi=pmi2 python $APPS/bsfh/scripts/prospector.py --param_file=$APPS/threedhst_bsfh/parameter_files/edo/edo_params.py 
