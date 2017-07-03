aed4f574b183c07fd1914cd0bb94e011ebc4c8ef
Joel Leja <joel.leja@gmail.com>
2017-07-03 17:48:26 -0400

    No AGB dust run

---
+++ batch_jobs/td_massive_noagb_secondary.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 960
+### Partition or queue name
+#SBATCH -p conroy,serial_requeue
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=6500
+### Job name
+#SBATCH -J '3d_mass_sec'
+### output and error logs
+#SBATCH -o td_massive_noagb_sec_%a.out
+#SBATCH -e td_massive_noagb_sec_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+IDFILE=$APPS"/threedhst_bsfh/data/3dhst/td_massive.ids"
+OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+
+python $APPS/threedhst_bsfh/code/extra_output.py \
+$APPS/threedhst_bsfh/parameter_files/td_massive_noagb_params.py \
+--outname="$APPS"/threedhst_bsfh/results/td_massive_noagb/"$OBJID"
---
+++ batch_jobs/td_massive_noagb_suball.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 32
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 1440
+### Partition or queue name
+#SBATCH -p conroy,general,conroy-intel
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=3000
+### Job name
+#SBATCH -J 'td_mass'
+### output and error logs
+#SBATCH -o td_massive_noagb_%a.out
+#SBATCH -e td_massive_noagb_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+IDFILE=$APPS"/threedhst_bsfh/data/3dhst/td_massive.ids"
+OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+srun -n $SLURM_NTASKS --mpi=pmi2 python $APPS/bsfh/scripts/prospector.py \
+--param_file="$APPS"/threedhst_bsfh/parameter_files/td_massive_noagb_params.py \
+--objname="$OBJID" \
+--outfile="$APPS"/threedhst_bsfh/results/td_massive_noagb/"$OBJID"
