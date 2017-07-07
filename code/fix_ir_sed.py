28c4e1a4e8178c876af7c9687b4c27bfd1ff91ef
Joel Leja <joel.leja@gmail.com>
2017-07-06 20:01:09 -0400

    new development

---
+++ code/fix_ir_sed.py
@@ -0,0 +1,123 @@
+import numpy as np
+import prosp_dutils, brown_io, copy
+import os
+import td_massive_params as pfile
+from astropy import constants
+from astropy.cosmology import WMAP9
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+import os
+
+plt.ioff()
+pc = 3.085677581467192e18  # in cm
+dfactor_10pc = 4*np.pi*(10*pc)**2
+to_ergs = 3631e-23
+
+dale_helou_txt = '/Users/joel/code/python/threedhst_bsfh/data/MIPS/dale_helou.txt'
+with open(dale_helou_txt, 'r') as f: hdr = f.readline().split()[1:]
+conversion = np.loadtxt(dale_helou_txt, comments = '#', dtype = np.dtype([(n, np.float) for n in hdr]))
+
+outdir = '/Users/joel/code/python/threedhst_bsfh/tests/irsed/'
+
+def dl07_to_dh02():
+
+    '''
+    This calculates UV + IR SFRs based on the Dale & Helou IR templates & MIPS flux
+    '''
+
+    ## build model components
+    run_params = pfile.run_params
+    sps = pfile.load_sps(**run_params)
+    model = pfile.load_model(**run_params)
+    obs = pfile.load_obs(**run_params)
+    mips_idx = [f.name for f in obs['filters']].index('mips_24um_aegis')
+    model.params['nebemlineinspec'] = True # yeah this is necessary right now
+
+    ## generate default model
+    theta = copy.copy(model.initial_theta)
+    pnames = model.theta_labels()
+    theta[pnames.index('duste_gamma')] = 0.01
+    theta[pnames.index('duste_umin')] = 1.0
+    qpah_grid = np.linspace(0,10,101)
+
+    ### generate Prospector L_IR
+    # this should remain constant with redshift
+    # input angstroms, Lsun/Hz. output in erg/s, convert to Lsun
+    model.params['zred'] = 0.0
+    spec,mags,sm = model.mean_model(theta, obs, sps=sps) # everything in maggies
+    spec *= dfactor_10pc / constants.L_sun.cgs.value * to_ergs
+    lir = prosp_dutils.return_lir(sps.wavelengths,spec)/constants.L_sun.cgs.value
+
+    ## range of redshifts
+    zrange = np.linspace(0.1,3,50)
+
+    mips_to_lir_dh, mips_to_lir_prosp, qpah_min = [], [], []
+    for i, z in enumerate(zrange):
+        print i
+        #### generate observed MIPS flux
+        model.params['zred'] = z
+        obs_mips_mjy, lir_mips = [], []
+        for qpah in qpah_grid:
+            theta[pnames.index('duste_qpah')] = qpah
+            spec,mags,sm = model.mean_model(theta, obs, sps=sps) # everything in maggies
+            obs_mips_mjy.append(mags[mips_idx] * 3631 * 1e3)
+ 
+            ### goes in in milliJy, comes out in Lsun
+            lir_mips.append(mips_to_lir(obs_mips_mjy[-1],z))
+
+        minidx = np.abs(lir_mips-lir).argmin()
+
+        ### output in milliJy / Lsun
+        mips_to_lir_dh.append(lir_mips[minidx]/obs_mips_mjy[minidx])
+        mips_to_lir_prosp.append(lir/obs_mips_mjy[minidx])
+        qpah_min.append(qpah_grid[minidx])
+
+    ### quick plotting routine
+    fig, ax = plt.subplots(1,3, figsize = (15,5))
+    xplot = zrange
+    yplot = [np.array(mips_to_lir_dh),np.array(mips_to_lir_prosp)]
+    color = ['blue','green']
+    label = ['Dale & Helou 02', 'DL07 (Prospector)']
+
+    for i, yp in enumerate(yplot): ax[0].plot(xplot, np.log10(yp), 'o',alpha=0.6,linestyle='-',color=color[i],lw=2,label=label[i])
+    ax[1].plot(xplot, np.log10(yplot[0])-np.log10(yplot[1]), 'o',alpha=0.6,linestyle='-',color=color[i],lw=2,label=label[i])
+    ax[2].plot(xplot, qpah_min, 'o', alpha=0.6, linestyle='-', color='0.3',lw=2)
+
+    for a in ax: a.set_xlabel('redshift')
+    ax[0].set_ylabel('log(LIR / MIPS flux)')
+    ax[1].set_ylabel(r'$\Delta$ (DH02 - DL07)')
+    ax[2].set_ylabel(r'Q$_{\mathrm{PAH}}$(min)')
+
+    ax[0].legend(loc=0, fontsize=12, prop={'size':12}, frameon=True,numpoints=1)
+    ax[1].axhline(0, linestyle='--', color='0.5',lw=2,zorder=-1)
+    ax[2].set_ylim(qpah_grid[0]-0.5,qpah_grid[-1]+0.5)
+
+    plt.tight_layout()
+    plt.savefig(outdir+'qpah_with_zred.png', dpi=150)
+    plt.close()
+
+    np.savetxt(outdir+'qpah.txt', np.transpose([zrange,qpah_min]), fmt='%1.4f', header='z qpah')
+
+def mips_to_lir(mips_flux,z):
+
+    '''
+    input flux must be in mJy
+    output is in Lsun
+    L_IR [Lsun] = fac_<band>(redshift) * flux [milliJy]
+    '''
+    
+    # if we're at higher redshift, interpolate
+    # it decrease error due to redshift relative to rest-frame template (good)
+    # but adds nonlinear error due to distances (bad)
+    # else, scale the nearest conversion factor by the 
+    # ratio of luminosity distances, since nonlinear error due to distances will dominate
+    if z > 0.1:
+        intfnc = interp1d(conversion['redshift'],conversion['fac_MIPS24um'], bounds_error = True, fill_value = 0)
+        fac = intfnc(z)
+    else:
+        near_idx = np.abs(conversion['redshift']-z).argmin()
+        lumdist_ratio = (WMAP9.luminosity_distance(z).value / WMAP9.luminosity_distance(conversion['redshift'][near_idx]).value)**2
+        zfac_ratio = (1.+conversion['redshift'][near_idx]) / (1.+z)
+        fac = conversion['fac_MIPS24um'][near_idx]*lumdist_ratio*zfac_ratio
+
+    return fac*mips_flux
---
+++ code/td/td_allpar.py
@@ -0,0 +1,213 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import prosp_dutils
+import magphys_plot_pref
+from matplotlib.ticker import MaxNLocator
+import copy, os, pickle
+import matplotlib as mpl
+from brown_io import load_prospector_extra
+
+mpl.rcParams.update({'font.size': 18})
+mpl.rcParams.update({'font.weight': 500})
+mpl.rcParams.update({'axes.labelweight': 500})
+
+def load_alldata(runname,filename,regenerate=False):
+
+    if regenerate or not os.path.isfile(filename):
+        basenames, _, _ = prosp_dutils.generate_basenames(runname)
+        alldata = []
+        for i, name in enumerate(basenames):
+
+            # prospector first
+            try:
+                prosp = load_prospector_extra(name)
+            except TypeError:
+                print name.split('/')[-1]+' failed to load. skipping.'
+                continue
+            dat = {'quantiles':prosp['quantiles'],'extras':prosp['extras']}
+            alldata.append(dat)
+        pickle.dump(alldata,open(filename, "w"))
+    else:
+        with open(filename, "r") as f:
+            alldata=pickle.load(f)
+    return alldata
+
+def arrange_data(alldata):
+
+    ### normal parameter labels
+    parnames_all = alldata[0]['quantiles']['parnames']
+    parnames = [par for par in parnames_all if 'z_fraction' not in par]
+
+    ### extra parameters
+    eparnames_all = alldata[0]['extras']['parnames']
+    eparnames = ['sfr_100', 'ssfr_100', 'half_time']
+
+    parlabels = {
+                 'logmass':r'log(M/M$_{\odot}$)', 
+                 'dust2': r'diffuse dust', 
+                 'logzsol': r'log(Z/Z$_{\odot}$)', 
+                 'dust_index': r'diffuse dust index',
+                 'dust1_fraction': r'(diffuse/young) dust', 
+                 'duste_qpah': r'Q$_{\mathrm{PAH}}$',
+                 'fagn': r'log(f$_{\mathrm{AGN}}$)',
+                 'agn_tau': r'$\tau_{\mathrm{AGN}}$',
+                 'sfr_100': r'log(SFR) [100 Myr]',
+                 'ssfr_100': r'log(sSFR) [100 Myr]',
+                 'half_time': r"log(t$_{\mathrm{half-mass}})$ [Gyr]"
+                 }
+
+    ### setup dictionary
+    outvals, outq, outerrs, outlabels = {},{},{},{}
+    for ii,par in enumerate(parnames): 
+        outvals[par] = []
+        outq[par] = {}
+        outq[par]['q50'],outq[par]['q84'],outq[par]['q16'] = [],[],[]
+    for ii,par in enumerate(eparnames):
+        outvals[par] = []
+        outq[par] = {}
+        outq[par]['q50'],outq[par]['q84'],outq[par]['q16'] = [],[],[]
+
+    ### fill with data
+    for dat in alldata:
+        for ii,par in enumerate(parnames):
+            match = parnames_all == par
+            if par == 'fagn':
+                for q in outq[par].keys(): outq[par][q].append(np.log10(dat['quantiles'][q][match]))
+                outvals[par].append(np.log10(dat['quantiles']['q50'][match]))
+            else:
+                for q in outq[par].keys(): outq[par][q].append(dat['quantiles'][q][match])
+                outvals[par].append(dat['quantiles']['q50'][match])
+        for par in eparnames:
+            match = eparnames_all == par
+            for q in outq[par].keys(): outq[par][q].append(np.log10(dat['extras'][q][match][0]))
+            outvals[par].append(np.log10(dat['extras']['q50'][match][0]))
+
+    ### do the errors
+    for par in parlabels.keys():
+        outerrs[par] = prosp_dutils.asym_errors(np.array(outq[par]['q50']), 
+                                                np.array(outq[par]['q84']),
+                                                np.array(outq[par]['q16']),log=False)
+        outvals[par] = np.array(outvals[par])
+
+    ### fill output
+    out = {}
+    out['median'] = outvals
+    out['errs'] = outerrs
+    out['parlabels'] = parlabels
+    out['ordered_labels'] = np.array(parnames + eparnames)
+
+    return out
+    
+def allpar_plot(runname='td_massive',outfolder=None,lowmet=True,regenerate=False):
+
+    ### I/O stuff
+    if outfolder is None:
+        outfolder = os.getenv('APPS') + '/threedhst_bsfh/plots/'+runname+'/fast_plots/'
+    if not os.path.isdir(outfolder):
+        os.makedirs(outfolder)
+        os.makedirs(outfolder+'data/')
+    filename=outfolder+'data/allpar_plot.h5'
+
+    ### load data
+    alldata = load_alldata(runname,filename,regenerate=regenerate)
+    dat = arrange_data(alldata)
+    npars = len(dat['parlabels'].keys())
+
+    ### plot preferences
+    fig, ax = plt.subplots(ncols=npars, nrows=npars, figsize=(npars*3,npars*3))
+    fig.subplots_adjust(wspace=0.0,hspace=0.0,top=0.95,bottom=0.05,left=0.05,right=0.95)
+    opts = {
+            'color': '#1C86EE',
+            'mew': 1.5,
+            'alpha': 0.6,
+            'fmt': 'o'
+           }
+    hopts = copy.deepcopy(opts)
+    hopts['color'] = '#FF420E'
+    outname = outfolder+'all_parameter.png'
+
+    ### color low-metallicity, high-mass galaxies
+    # hijack Herschel flag
+    if lowmet:
+        parnames = alldata[0]['quantiles']['parnames']
+        met_idx = parnames == 'logzsol'
+        mass_idx = parnames == 'logmass'
+        met_q50 = np.array([data['quantiles']['q50'][met_idx][0] for data in alldata])
+        mass_q50 = np.array([data['quantiles']['q50'][mass_idx][0] for data in alldata])
+
+        hflag = (met_q50 < -1.0) & (mass_q50 > 9.5)
+
+        outname = outfolder+'all_parameter_lowmet.png'
+
+    else:
+        hflag = np.ones_like(alldata[0]['quantiles']['q50'])
+
+    for yy, ypar in enumerate(dat['ordered_labels']):
+        for xx, xpar in enumerate(dat['ordered_labels']):
+
+            # turn off the dumb ones
+            if xx >= yy:
+                ax[yy,xx].axis('off')
+                continue
+
+            ax[yy,xx].errorbar(dat['median'][xpar][~hflag],dat['median'][ypar][~hflag],
+                               xerr=[dat['errs'][xpar][0][~hflag],dat['errs'][xpar][1][~hflag]], 
+                               yerr=[dat['errs'][ypar][0][~hflag],dat['errs'][ypar][1][~hflag]], 
+                               **opts)
+
+            ax[yy,xx].errorbar(dat['median'][xpar][hflag],dat['median'][ypar][hflag],
+                               xerr=[dat['errs'][xpar][0][hflag],dat['errs'][xpar][1][hflag]], 
+                               yerr=[dat['errs'][ypar][0][hflag],dat['errs'][ypar][1][hflag]], 
+                               **hopts)
+
+            #### RANGE
+            minx,maxx = dat['median'][xpar].min(),dat['median'][xpar].max()
+            dynx = (maxx-minx)*0.1
+            ax[yy,xx].set_xlim(minx-dynx, maxx+dynx)
+
+            miny,maxy = dat['median'][ypar].min(),dat['median'][ypar].max()
+            dyny = (maxy-miny)*0.1
+            ax[yy,xx].set_ylim(miny-dyny, maxy+dyny)
+
+            #### LABELS
+            if xx % npars == 0:
+                ax[yy,xx].set_ylabel(dat['parlabels'][ypar])
+            else:
+                for tl in ax[yy,xx].get_yticklabels():tl.set_visible(False)
+
+            if yy == npars-1:
+                ax[yy,xx].set_xlabel(dat['parlabels'][xpar])
+            else:
+                for tl in ax[yy,xx].get_xticklabels():tl.set_visible(False)
+
+            ax[yy,xx].xaxis.set_major_locator(MaxNLocator(5))
+            ax[yy,xx].yaxis.set_major_locator(MaxNLocator(5))
+
+            plt.setp(ax[yy,xx].xaxis.get_majorticklabels(), rotation=-55, horizontalalignment='center')
+
+    plt.savefig(outname,dpi=100)
+    plt.close()
+
+    mpl.rcParams.update({'font.weight': 400})
+    mpl.rcParams.update({'axes.labelweight': 400})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
