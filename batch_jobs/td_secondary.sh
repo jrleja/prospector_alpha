56a7be41234a09e8d3e9dea1428a85488bc6e88f
Joel Leja <joel.leja@gmail.com>
2017-09-14 15:59:07 -0400

    here we go...

---
+++ batch_jobs/td_secondary.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 960
+### Partition or queue name
+#SBATCH -p conroy,serial_requeue
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=6500
+### Job name
+#SBATCH -J 'td_sec_%a'
+### output and error logs
+#SBATCH -o td_sec_%a.out
+#SBATCH -e td_sec_%a.err
+### mail
+#SBATCH --mail-type=END
+#SBATCH --mail-user=joel.leja@gmail.com
+IDFILE=$APPS"/prospector_alpha/data/3dhst/td.ids"
+OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+
+python $APPS/prospector_alpha/code/extra_output.py \
+$APPS/prospector_alpha/parameter_files/td_params.py \
+--outname="$APPS"/prospector_alpha/results/td/"$OBJID"
