14f92994eba059837afe32703e6db0d3f6f7d835
Joel Leja <joel.leja@gmail.com>
2017-11-06 15:23:46 -0500

    final tweaks

--- batch_jobs/submit_missing_td_huge.sh
+++ batch_jobs/submit_missing_td_huge.sh
@@ -6,12 +6,11 @@ IDFILE=$APPS"/prospector_alpha/data/3dhst/td_huge.ids"
 
 for value in {1..54418}
 do
-    OBJID=$(sed -n "${n}p" "$IDFILE")
+    OBJID=$(sed -n "${value}p" "$IDFILE")
     FIELD=${OBJID%_*}
-    mod=${OBJID//[ ]/*}
-    if ! ls $APPS/prospector_alpha/results/td_huge/$field/*$mod*model 1> /dev/null 2>&1; then
-        OUTFILE="$APPS"/prospector_alpha/results/td_huge/"${FIELD}"/"${OBJID}"
-        echo ${OBJID} ${OUTFILE} $value
+    if ! ls $APPS/prospector_alpha/results/td_huge/$FIELD/${OBJID}_*_model 1> /dev/null 2>&1; then
+        OUTFILE=$APPS/prospector_alpha/results/td_huge/${FIELD}/${OBJID}
+        echo ${OBJID}, ${value}
         sbatch -o td_huge_${value}.out -e td_huge_${value}.err submit_one_td_huge.sh ${OBJID} ${OUTFILE}
     fi
 done
--- batch_jobs/submit_one_td_huge.sh
+++ batch_jobs/submit_one_td_huge.sh
@@ -12,11 +12,8 @@
 #SBATCH --mem-per-cpu=4000
 ### Job name
 #SBATCH -J 'td'
-objname=${1}_${2}
-outfile="$APPS"/prospector_alpha/results/td_huge/"${1}"/"${objname}"
-echo ${objname},${outfile},$3
-
+echo ${1} ${2}
 srun -n 1 --mpi=pmi2 python $APPS/prospector/scripts/prospector_dynesty.py \
 --param_file="$APPS"/prospector_alpha/parameter_files/td_huge_params.py \
---objname="${objname}" \
---outfile="${outfile}"
+--objname="${1}" \
+--outfile="${2}"
