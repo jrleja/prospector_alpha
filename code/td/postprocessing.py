434f498977c2de22f240adebdce14709140fcfbb
Joel Leja <joel.leja@gmail.com>
2018-02-25 10:56:54 -0500

    parenthesis around string logic

--- code/td/postprocessing.py
+++ code/td/postprocessing.py
@@ -237,7 +237,7 @@ def post_processing(param_name, objname=None, runname = None, overwrite=True, ob
             obj_outfile = "/".join(run_outfile.split('/')[:-2]) + '/' + runname + '/' + objname
 
         # account for unique td_huge storage situation
-        if runname == 'td_huge' | runname == 'td_new':
+        if (runname == 'td_huge') | (runname == 'td_new'):
             field = obj_outfile.split('/')[-1].split('_')[0]
             obj_outfile = "/".join(obj_outfile.split('/')[:-1])+'/'+field+'/'+obj_outfile.split('/')[-1]  
 
