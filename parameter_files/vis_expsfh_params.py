7be8c49a6eacd8bcf6f51cf4757ceeac7c874d67
Joel Leja <joel.leja@gmail.com>
2018-03-08 12:17:54 -0500

    add vis_expsfh

---
+++ parameter_files/vis_expsfh_params.py
@@ -0,0 +1,438 @@
+import numpy as np
+import os
+from prospect.models import priors, sedmodel
+from prospect.sources import CSPSpecBasis
+from sedpy import observate
+from astropy.cosmology import WMAP9
+from td_io import load_zp_offsets
+from scipy.stats import truncnorm
+from astropy.io import ascii
+
+lsun = 3.846e33
+pc = 3.085677581467192e18  # in cm
+
+lightspeed = 2.998e18  # AA/s
+to_cgs = lsun/(4.0 * np.pi * (pc*10)**2)
+jansky_mks = 1e-26
+
+#############
+# RUN_PARAMS
+#############
+APPS = os.getenv('APPS')
+run_params = {'verbose':True,
+              'debug': False,
+              'outfile': APPS+'/prospector_alpha/results/vis/vis_expsfh_1',
+              'nofork': True,
+              # dynesty params
+              'nested_bound': 'multi', # bounding method
+              'nested_sample': 'rwalk', # sampling method
+              'nested_walks': 50, # MC walks
+              'nested_nlive_batch': 200, # size of live point "batches"
+              'nested_nlive_init': 200, # number of initial live points
+              'nested_weight_kwargs': {'pfrac': 1.0}, # weight posterior over evidence by 100%
+              'nested_dlogz_init': 0.01,
+              'nested_stop_kwargs': {'post_thresh': 0.015, 'n_mc':50}, #higher threshold, more MCMC
+              # Model info
+              'zcontinuous': 2,
+              'compute_vega_mags': False,
+              'initial_disp':0.1,
+              'interp_type': 'logarithmic',
+              # Data info (phot = .cat, dat = .dat, fast = .fout)
+              'objname':'vis_expsfh_1',
+              'filter_key':10
+              }
+############
+# OBS
+#############
+def load_obs(filter_key=None,**extras):
+    from vis_params import load_obs
+    return load_obs(filter_key=filter_key)
+
+##########################
+# TRANSFORMATION FUNCTIONS
+##########################
+def load_gp(**extras):
+    return None, None
+
+def transform_logmass_to_mass(mass=None, logmass=None, **extras):
+    return 10**logmass
+
+def transform_logtau_to_tau(tau=None, logtau=None, **extras):
+    return 10**logtau
+#############
+# MODEL_PARAMS
+#############
+model_params = []
+
+###### BASIC PARAMETERS ##########
+model_params.append({'name': 'zred', 'N': 1,
+                        'isfree': False,
+                        'init': 0.0001,
+                        'units': '',
+                        'prior': priors.TopHat(mini=0.0, maxi=4.0)})
+
+model_params.append({'name': 'add_igm_absorption', 'N': 1,
+                        'isfree': False,
+                        'init': 1,
+                        'units': None,
+                        'prior_function': None,
+                        'prior_args': None})
+
+model_params.append({'name': 'add_agb_dust_model', 'N': 1,
+                        'isfree': False,
+                        'init': True,
+                        'units': None,
+                        'prior_function': None,
+                        'prior_args': None})
+
+model_params.append({'name': 'pmetals', 'N': 1,
+                        'isfree': False,
+                        'init': -99,
+                        'units': '',
+                        'prior_function': None,
+                        'prior_args': {'mini':-3, 'maxi':-1}})
+
+model_params.append({'name': 'logmass', 'N': 1,
+                        'isfree': True,
+                        'init': 10.0,
+                        'units': 'Msun',
+                        'prior': priors.TopHat(mini=5.0, maxi=13.0)})
+
+model_params.append({'name': 'mass', 'N': 1,
+                        'isfree': False,
+                        'init': 1e10,
+                        'depends_on': transform_logmass_to_mass,
+                        'units': 'Msun',
+                        'prior': priors.TopHat(mini=1e5, maxi=1e13)})
+
+model_params.append({'name': 'logzsol', 'N': 1,
+                        'isfree': True,
+                        'init': -0.5,
+                        'init_disp': 0.25,
+                        'disp_floor': 0.2,
+                        'units': r'$\log (Z/Z_\odot)$',
+                        'prior': priors.TopHat(mini=-1.98, maxi=0.19)})
+                        
+###### SFH   ########
+model_params.append({'name': 'sfh', 'N': 1,
+                        'isfree': False,
+                        'init': 1,
+                        'units': 'type',
+                        'prior_function_name': None,
+                        'prior_args': None})
+
+model_params.append({'name': 'tau', 'N': 1,
+                        'isfree': False,
+                        'init': 10,
+                        'depends_on': transform_logtau_to_tau,
+                        'init_disp': 0.5,
+                        'units': 'Gyr',
+                        'prior': priors.TopHat(mini=0.03, maxi=100)})
+
+model_params.append({'name': 'logtau', 'N': 1,
+                        'isfree': True,
+                        'init': 1,
+                        'init_disp': 0.5,
+                        'units': 'Gyr',
+                        'prior': priors.TopHat(mini=-1.52, maxi=2.0)})
+
+model_params.append({'name': 'tage', 'N': 1,
+                        'isfree': True,
+                        'init': 1.0,
+                        'units': 'Gyr',
+                        'prior': priors.TopHat(mini=0.01, maxi=14.0)})
+
+########    IMF  ##############
+model_params.append({'name': 'imf_type', 'N': 1,
+                             'isfree': False,
+                             'init': 1, #1 = chabrier
+                             'units': None,
+                             'prior': None})
+
+######## Dust Absorption ##############
+model_params.append({'name': 'dust_type', 'N': 1,
+                        'isfree': False,
+                        'init': 4,
+                        'units': 'index',
+                        'prior_function_name': None,
+                        'prior_args': None})
+                        
+model_params.append({'name': 'dust1', 'N': 1,
+                        'isfree': False,
+                        'depends_on': to_dust1,
+                        'init': 1.0,
+                        'units': '',
+                        'prior': None})
+
+model_params.append({'name': 'dust1_fraction', 'N': 1,
+                        'isfree': True,
+                        'init': 1.0,
+                        'init_disp': 0.8,
+                        'disp_floor': 0.8,
+                        'units': '',
+                        'prior': priors.ClippedNormal(mini=0.0, maxi=2.0, mean=1.0, sigma=0.3)})
+
+model_params.append({'name': 'dust2', 'N': 1,
+                        'isfree': True,
+                        'init': 1.0,
+                        'init_disp': 0.25,
+                        'disp_floor': 0.15,
+                        'units': '',
+                        'prior': priors.TopHat(mini=0.0, maxi=3.0)})
+
+model_params.append({'name': 'dust_index', 'N': 1,
+                        'isfree': True,
+                        'init': 0.0,
+                        'init_disp': 0.25,
+                        'disp_floor': 0.15,
+                        'units': '',
+                        'prior': priors.TopHat(mini=-1, maxi=0.4)})
+
+model_params.append({'name': 'dust1_index', 'N': 1,
+                        'isfree': False,
+                        'init': -1.0,
+                        'units': '',
+                        'prior': None})
+
+model_params.append({'name': 'dust_tesc', 'N': 1,
+                        'isfree': False,
+                        'init': 7.0,
+                        'units': 'log(Gyr)',
+                        'prior_function_name': None,
+                        'prior_args': None})
+
+###### Dust Emission ##############
+model_params.append({'name': 'add_dust_emission', 'N': 1,
+                        'isfree': False,
+                        'init': 1,
+                        'units': None,
+                        'prior': None})
+
+model_params.append({'name': 'duste_gamma', 'N': 1,
+                        'isfree': True,
+                        'init': 0.01,
+                        'init_disp': 0.4,
+                        'disp_floor': 0.3,
+                        'units': None,
+                        'prior': priors.TopHat(mini=0.0, maxi=1.0)})
+
+model_params.append({'name': 'duste_umin', 'N': 1,
+                        'isfree': True,
+                        'init': 1.0,
+                        'init_disp': 10.0,
+                        'disp_floor': 5.0,
+                        'units': None,
+                        'prior': priors.TopHat(mini=0.1, maxi=25.0)})
+
+model_params.append({'name': 'duste_qpah', 'N': 1,
+                        'isfree': True,
+                        'init': 3.0,
+                        'init_disp': 3.0,
+                        'disp_floor': 3.0,
+                        'units': 'percent',
+                        'prior': priors.TopHat(mini=0.0, maxi=10.0)})
+
+###### Nebular Emission ###########
+model_params.append({'name': 'add_neb_emission', 'N': 1,
+                        'isfree': False,
+                        'init': True,
+                        'units': r'log Z/Z_\odot',
+                        'prior': None})
+
+model_params.append({'name': 'add_neb_continuum', 'N': 1,
+                        'isfree': False,
+                        'init': True,
+                        'units': r'log Z/Z_\odot',
+                        'prior': None})
+
+model_params.append({'name': 'nebemlineinspec', 'N': 1,
+                        'isfree': False,
+                        'init': False,
+                        'prior': None})
+
+model_params.append({'name': 'gas_logz', 'N': 1,
+                        'isfree': False,
+                        'init': 0.0,
+                        'depends_on': tie_gas_logz,
+                        'units': r'log Z/Z_\odot',
+                        'prior': priors.TopHat(mini=-2.0, maxi=0.5)})
+
+model_params.append({'name': 'gas_logu', 'N': 1, # scale with sSFR?
+                        'isfree': False,
+                        'init': -2.0,
+                        'units': '',
+                        'prior': priors.TopHat(mini=-4.0, maxi=-1.0)})
+
+##### AGN dust ##############
+model_params.append({'name': 'add_agn_dust', 'N': 1,
+                        'isfree': False,
+                        'init': True,
+                        'units': '',
+                        'prior': None})
+
+model_params.append({'name': 'fagn', 'N': 1,
+                        'isfree': True,
+                        'init': 0.01,
+                        'init_disp': 0.03,
+                        'disp_floor': 0.02,
+                        'units': '',
+                        'prior': priors.LogUniform(mini=1e-4, maxi=3.0)})
+
+model_params.append({'name': 'agn_tau', 'N': 1,
+                        'isfree': True,
+                        'init': 20.0,
+                        'init_disp': 5,
+                        'disp_floor': 2,
+                        'units': '',
+                        'prior': priors.LogUniform(mini=5.0, maxi=150.0)})
+
+####### Calibration ##########
+model_params.append({'name': 'phot_jitter', 'N': 1,
+                        'isfree': False,
+                        'init': 0.0,
+                        'init_disp': 0.5,
+                        'units': 'fractional maggies (mags/1.086)',
+                        'prior': priors.TopHat(mini=0.0, maxi=0.5)})
+
+####### Units ##########
+model_params.append({'name': 'peraa', 'N': 1,
+                     'isfree': False,
+                     'init': False})
+
+model_params.append({'name': 'mass_units', 'N': 1,
+                     'isfree': False,
+                     'init': 'mformed'})
+
+#### resort list of parameters 
+# because we can
+parnames = [m['name'] for m in model_params]
+fit_order = ['logmass','tage','logzsol','logtau', 'dust2', 'dust_index', 'dust1_fraction', 'fagn', 'agn_tau','duste_qpah','duste_umin','duste_gamma']
+tparams = [model_params[parnames.index(i)] for i in fit_order]
+for param in model_params: 
+    if param['name'] not in fit_order:
+        tparams.append(param)
+model_params = tparams
+
+###### Redefine SPS ######
+class NebSFH(FastStepBasis):
+    
+    @property
+    def emline_wavelengths(self):
+        return self.ssp.emline_wavelengths
+
+    @property
+    def get_nebline_luminosity(self):
+        """Emission line luminosities in units of Lsun per solar mass formed
+        """
+        return self.ssp.emline_luminosity/self.params['mass'].sum()
+
+    def nebline_photometry(self,filters,z):
+        """analytically calculate emission line contribution to photometry
+        """
+        emlams = self.emline_wavelengths * (1+z)
+        elums = self.get_nebline_luminosity # Lsun / solar mass formed
+        flux = np.empty(len(filters))
+        for i,filt in enumerate(filters):
+            # calculate transmission at nebular emission
+            trans = np.interp(emlams, filt.wavelength, filt.transmission, left=0., right=0.)
+            idx = (trans > 0)
+            if True in idx:
+                flux[i] = (trans[idx]*emlams[idx]*elums[idx]).sum()/filt.ab_zero_counts
+            else:
+                flux[i] = 0.0
+        return flux
+
+    def get_spectrum(self, outwave=None, filters=None, peraa=False, **params):
+        """Get a spectrum and SED for the given params.
+        ripped from SSPBasis
+        addition: check for flag nebeminspec. if not true,
+        add emission lines directly to photometry
+        """
+
+        # Spectrum in Lsun/Hz per solar mass formed, restframe
+        wave, spectrum, mfrac = self.get_galaxy_spectrum(**params)
+
+        # Redshifting + Wavelength solution
+        # We do it ourselves.
+        a = 1 + self.params.get('zred', 0)
+        af = a
+        b = 0.0
+
+        if 'wavecal_coeffs' in self.params:
+            x = wave - wave.min()
+            x = 2.0 * (x / x.max()) - 1.0
+            c = np.insert(self.params['wavecal_coeffs'], 0, 0)
+            # assume coeeficients give shifts in km/s
+            b = chebval(x, c) / (lightspeed*1e-13)
+
+        wa, sa = wave * (a + b), spectrum * af  # Observed Frame
+        if outwave is None:
+            outwave = wa
+        
+        spec_aa = lightspeed/wa**2 * sa # convert to perAA
+        # Observed frame photometry, as absolute maggies
+        if filters is not None:
+            mags = observate.getSED(wa, spec_aa * to_cgs, filters)
+            phot = np.atleast_1d(10**(-0.4 * mags))
+        else:
+            phot = 0.0
+
+        ### if we don't have emission lines, add them
+        if (not self.params['nebemlineinspec']) and self.params['add_neb_emission']:
+            phot += self.nebline_photometry(filters,a-1)*to_cgs
+
+        # Spectral smoothing.
+        do_smooth = (('sigma_smooth' in self.params) and
+                     ('sigma_smooth' in self.reserved_params))
+        if do_smooth:
+            # We do it ourselves.
+            smspec = self.smoothspec(wa, sa, self.params['sigma_smooth'],
+                                     outwave=outwave, **self.params)
+        elif outwave is not wa:
+            # Just interpolate
+            smspec = np.interp(outwave, wa, sa, left=0, right=0)
+        else:
+            # no interpolation necessary
+            smspec = sa
+
+        # Distance dimming and unit conversion
+        zred = self.params.get('zred', 0.0)
+        if (zred == 0) or ('lumdist' in self.params):
+            # Use 10pc for the luminosity distance (or a number
+            # provided in the dist key in units of Mpc)
+            dfactor = (self.params.get('lumdist', 1e-5) * 1e5)**2
+        else:
+            lumdist = WMAP9.luminosity_distance(zred).value
+            dfactor = (lumdist * 1e5)**2
+        if peraa:
+            # spectrum will be in erg/s/cm^2/AA
+            smspec *= to_cgs / dfactor * lightspeed / outwave**2
+        else:
+            # Spectrum will be in maggies
+            smspec *= to_cgs / dfactor / 1e3 / (3631*jansky_mks)
+
+        # Convert from absolute maggies to apparent maggies
+        phot /= dfactor
+
+        # Mass normalization
+        mass = np.sum(self.params.get('mass', 1.0))
+        if np.all(self.params.get('mass_units', 'mstar') == 'mstar'):
+            # Convert from current stellar mass to mass formed
+            mass /= mfrac
+
+        return smspec * mass, phot * mass, mfrac
+
+def load_sps(**extras):
+
+    sps = NebSFH(**extras)
+    return sps
+
+def load_model(**extras):
+
+    # set tage_max, fix redshift
+    n = [p['name'] for p in model_params]
+    zred = 0.0001
+    tuniv = WMAP9.age(zred).value
+    model_params[n.index('tage')]['prior'].update(maxi=tuniv)
+
+    return sedmodel.SedModel(model_params)
