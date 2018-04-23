dec4cdabc68eb40b00414335769ba756c426880a
Joel Leja <joel.leja@gmail.com>
2018-04-23 17:51:09 -0400

    run Brown sample with high-z model + test QPAH=0

---
+++ batch_jobs/td_new_nopah_suball.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p shared,itc_cluster
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### output and error logs
+#SBATCH -o td_new_nopah_%a.out
+#SBATCH -e td_new_nopah_%a.err
+### Job name
+#SBATCH -J 'td_new_nopah'
+IDFILE=$APPS"/prospector_alpha/data/3dhst/ushaped.ids"
+OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+srun -n 1 --mpi=pmi2 python $APPS/prospector/scripts/prospector_dynesty.py \
+--param_file="$APPS"/prospector_alpha/parameter_files/td_new_nopah_params.py \
+--objname="$OBJID" \
+--outfile="$APPS"/prospector_alpha/results/td_new_nopah/"$OBJID" \
+--runname="td_new"
