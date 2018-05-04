62bf66f86b4646979c43d6d3e9ac8c4c635bfac4
Joel Leja <joel.leja@gmail.com>
2018-05-04 13:44:10 -0400

    u-shaped SFHs: a final solution

---
+++ batch_jobs/td_new_alpha_suball.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+### Name of the job
+### Requested number of cores
+#SBATCH -n 1
+### Requested number of nodes
+#SBATCH -N 1
+### Requested computing time in minutes
+#SBATCH -t 10080
+### Partition or queue name
+#SBATCH -p shared,itc_cluster
+### memory per cpu, in MB
+#SBATCH --mem-per-cpu=4000
+### output and error logs
+#SBATCH -o td_new_alpha_%a.out
+#SBATCH -e td_new_alpha_%a.err
+### Job name
+#SBATCH -J 'td_new_alpha'
+IDFILE=$APPS"/prospector_alpha/data/3dhst/ushaped.ids"
+OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+srun -n 1 --mpi=pmi2 python $APPS/prospector/scripts/prospector_dynesty.py \
+--param_file="$APPS"/prospector_alpha/parameter_files/td_new_alpha_params.py \
+--objname="$OBJID" \
+--outfile="$APPS"/prospector_alpha/results/td_new_alpha/"$OBJID" \
+--runname="td_new"
