66b9ef04bbcb548425b98e034a4a244b66daad19
Joel Leja <joel.leja@gmail.com>
2018-07-03 12:51:03 -0400

    sdss lensed galaxy fits

--- batch_jobs/mo_secondary.sh
+++ batch_jobs/mo_secondary.sh
@@ -7,7 +7,7 @@
 ### Requested computing time in minutes
 #SBATCH -t 960
 ### Partition or queue name
-#SBATCH -p conroy,shared,serial_requeue
+#SBATCH -p conroy,shared,serial_requeue,itc_cluster,conroy-intel
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=4000
 ### Job name
@@ -16,11 +16,13 @@
 ### mail
 #SBATCH --mail-type=END
 #SBATCH --mail-user=joel.leja@gmail.com
-IDFILE=$APPS"/prospector_alpha/data/3dhst/mo.ids"
-OBJID=$(sed -n "${n}p" "$IDFILE")
+IDFILE=$APPS"/prospector_alpha/data/3dhst/mo.cat"
+n1=`expr $SLURM_ARRAY_TASK_ID + 1`
+OBJID=$(awk -v i=$n1 -v j=1 'FNR == i {print $j}' $IDFILE)
+
 srun -n 1 --exclusive --mpi=pmi2 python $APPS/prospector_alpha/code/td/postprocessing.py \
 $APPS/prospector_alpha/parameter_files/mo_params.py \
 --objname="$OBJID" \
 --overwrite=True \
---plot=False \
+--plot=True \
 --shorten_spec=True
--- batch_jobs/mo_suball.sh
+++ batch_jobs/mo_suball.sh
@@ -7,7 +7,7 @@
 ### Requested computing time in minutes
 #SBATCH -t 10080
 ### Partition or queue name
-#SBATCH -p shared
+#SBATCH -p conroy-intel,shared,itc_cluster
 ### memory per cpu, in MB
 #SBATCH --mem-per-cpu=4000
 ### output and error logs
@@ -15,8 +15,10 @@
 #SBATCH -e mo_%a.err
 ### Job name
 #SBATCH -J 'mo'
-IDFILE=$APPS"/prospector_alpha/data/3dhst/mo.ids"
-OBJID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$IDFILE")
+IDFILE=$APPS"/prospector_alpha/data/3dhst/mo.cat"
+n1=`expr $SLURM_ARRAY_TASK_ID + 1`
+OBJID=$(awk -v i=$n1 -v j=1 'FNR == i {print $j}' $IDFILE)
+
 srun -n 1 --mpi=pmi2 python $APPS/prospector/scripts/prospector_dynesty.py \
 --param_file="$APPS"/prospector_alpha/parameter_files/mo_params.py \
 --objname="$OBJID" \
