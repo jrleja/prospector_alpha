fixes to postprocessing and numerical issues in continuity prior

code/td/postprocessing.py
@@ -169,7 +169,7 @@ def calc_extra_quantities(res, sps, obs, ncalc=3000, shorten_spec=True, measure_
 
         # isolate young star contribution
         nodep_model.params['mass'] = np.zeros_like(res['model'].params['mass'])
-        nodep_model.params['mass'][0] = res['model'].params['mass'][0]
+        nodep_model.params['mass'][:1] = res['model'].params['mass'][:1]
         out = prosp_dutils.measure_restframe_properties(sps, model = nodep_model, thetas = nagn_thetas, measure_ir = True, measure_luv = True)
         eout['extras']['luv_young']['chain'][jj] = out['luv']
         eout['extras']['lir_young']['chain'][jj] = out['lir']
@@ -241,7 +241,7 @@ def post_processing(param_name, objname=None, runname = None, overwrite=True, ob
         os.makedirs(plot_outfolder)
 
     # I/O
-    res, powell_results, model, eout = load_prospector_data(obj_outfile,hdf5=True,load_extra_output=True)
+    res, powell_results, _, eout = load_prospector_data(obj_outfile,hdf5=True,load_extra_output=True)
     if res is None:
         print 'there are no sampling results! returning.'
         return

parameter_files/mock_fixed_student_params.py
@@ -95,7 +95,7 @@ def to_dust1(dust1_fraction=None, dust1=None, dust2=None, **extras):
     return dust1_fraction*dust2
 
 def logmass_to_masses(logmass=None, logsfr_ratios=None, agebins=None, **extras):
-    logsfr_ratios = np.clip(logsfr_ratios,-100,100) # numerical issues...
+    logsfr_ratios = np.clip(logsfr_ratios,-10,10) # numerical issues...
     nbins = agebins.shape[0]
     sratios = 10**logsfr_ratios
     dt = (10**agebins[:,1]-10**agebins[:,0])
@@ -330,14 +330,6 @@ for param in model_params:
     if param['name'] not in fit_order:
         tparams.append(param)
 model_params = tparams
-
-##### log(SFR_ratio) prior ######
-class SFR_Ratio(priors.Normal):
-    """A lognormal prior on the ratio of SFRs
-    to make (mean,sigma) functions of time, I need to write a function
-    that returns (t,dt) given input SFR_ratios. this should make use of the above transforms.
-    """
-    pass
 
 ###### Redefine SPS ######
 class NebSFH(FastStepBasis):

parameter_files/mock_student_params.py
@@ -72,8 +72,8 @@ def to_dust1(dust1_fraction=None, dust1=None, dust2=None, **extras):
     return dust1_fraction*dust2
 
 def logmass_to_masses(logmass=None, logsfr_ratio30=None, logsfr_ratiomax=None, agebins=None, **extras):
-    logsfr_ratio30 = np.clip(logsfr_ratio30,-100,100)
-    logsfr_ratiomax = np.clip(logsfr_ratiomax,-100,100)
+    logsfr_ratio30 = np.clip(logsfr_ratio30,-10,10)
+    logsfr_ratiomax = np.clip(logsfr_ratiomax,-10,10)
 
     nbins = agebins.shape[0]-2
     s30, smax = 10**logsfr_ratio30, 10**logsfr_ratiomax
@@ -96,8 +96,8 @@ def logsfr_ratios_to_agebins(logsfr_ratios=None, tuniv=None, **extras):
         where Sn = SFR(n) / SFR(n+1) and delta(t1) is width of youngest bin
     """
 
-    # clip to 5 dex for numerical stability
-    logsfr_ratios = np.clip(logsfr_ratios,-5,5)
+    # clip to 10 dex for numerical stability
+    logsfr_ratios = np.clip(logsfr_ratios,-10,10)
 
     # calculate delta(t) for the first bin
     lower_time = 5e7

parameter_files/td_delta_params.py
@@ -158,7 +158,7 @@ def massmet_to_logzsol(massmet=None,**extras):
     return massmet[1]
 
 def logmass_to_masses(massmet=None, logsfr_ratios=None, agebins=None, **extras):
-    logsfr_ratios = np.clip(logsfr_ratios,-100,100) # numerical issues...
+    logsfr_ratios = np.clip(logsfr_ratios,-10,10) # numerical issues...
     nbins = agebins.shape[0]
     sratios = 10**logsfr_ratios
     dt = (10**agebins[:,1]-10**agebins[:,0])