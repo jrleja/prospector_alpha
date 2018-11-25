39c0c489153f4f5aba49b58fa68f10ea42da168a
Joel Leja <joel.leja@gmail.com>
2018-11-26 04:59:21 +0900

    fix uvj dirichlet

--- parameter_files/uvj_dirichlet_params.py
+++ parameter_files/uvj_dirichlet_params.py
@@ -169,7 +169,7 @@ def zfrac_to_sfrac(z_fraction=None, **extras):
     sfr_fraction[0] = 1.0 - z_fraction[0]
     for i in range(1, len(z_fraction)):
         sfr_fraction[i] = np.prod(z_fraction[:i]) * (1.0 - z_fraction[i])
-    sfr_fraction[-1] = 1 - np.sum(sfr_fraction[:-1])
+    sfr_fraction[-1] = np.clip(1 - np.sum(sfr_fraction[:-1]),0,np.inf)
 
     return sfr_fraction
 
